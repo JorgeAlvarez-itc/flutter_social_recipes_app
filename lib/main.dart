b39c19ecf75b70506100434c1a936ce437cb37f7
Jorge <19030136@itcelaya.edu.mx>
2023-05-20 12:08:11 -0600

    notificaciones firebase funcionando al 10000%

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:recetas/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:recetas/screens/onboarding_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:recetas/firebase/firebase_notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAppCheck.instance.activate();
+  FireNotifications.initializeApp();
   runApp(const MyApp());
 }
 
